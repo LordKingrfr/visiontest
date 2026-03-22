b28d67250c5551349338b10cccd11c661b81dafb
LordKingrfr <81722065+LordKingrfr@users.noreply.github.com>
2026-03-22 18:20:46 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        brand: {
+          purple: '#a855f7',
+          dark: '#050505',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
